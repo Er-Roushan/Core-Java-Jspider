ae2c32f9e840a95e10db976adbcbde980ea21b10
Roushan Kumar <erroushankrofficial@gmail.com>
2024-07-27 14:49:57 +0530

    File addition

---
+++ Roushan Kumar/Part-3/Collection-Implementation/src/array_list/Sachin.java
@@ -0,0 +1,20 @@
+package array_list;
+import java.util.*;
+public class Sachin {
+	public static void main(String... args) {
+//		List li = new ArrayList();
+//		li.add("Hello");
+//		li.add("Madam");
+//		ListIterator i = li.listIterator();
+//		while(i.hasNext()) {
+//			i.add("Hi");
+//			i.next();
+//			li.add("Hll");
+//		}
+//		System.out.println(li);
+		
+		Integer a = new Integer(3);
+		int j=a;
+//		System.out.println(j.toString());
+	}
+}
